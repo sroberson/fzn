2c74baac6020911488c80de03dc20703d9b0ec59
Scott Roberson <sroberson@gmail.com>
2015-05-21 17:55:16 -0400

    making the complete callback call the stop method

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,19 @@
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0" + hours;}
+    if (minutes < 10) {minutes = "0" + minutes;}
+    if (seconds < 10) {seconds = "0" + seconds;}
+    var time    = hours + ':' + minutes + ':' + seconds;
+    return time;
+}
+
 var myMedia = null;
 var playing = false;
+var mediaTimer;
 
 document.addEventListener('deviceready', onDeviceReady, false);
 
@@ -10,6 +24,11 @@ function getMediaURL(s) {
 
 function playAudio() {
     if (!playing) {
+
+        if (myMedia === null) {
+            updateMedia();
+        }
+
         myMedia.play();
         document.getElementById('play').innerHTML = "Pause";
         playing = true;
@@ -22,6 +41,7 @@ function playAudio() {
 
 function stopAudio() {
     myMedia.stop();
+    clearInterval(mediaTimer);
     playing = false;
     document.getElementById('play').innerHTML = "Play";
     document.getElementById('audio_position').innerHTML = "0.000 sec";
@@ -33,23 +53,28 @@ function mediaError(e) {
 }
 
 function updateMedia(src) {
+
+    var positionVal;
+
     // Clean up old file
     if (myMedia != null) {
         myMedia.release();
     }
 
     // Get the media file
-    var mp3URL = getMediaURL("sounds/fzn15.mp3");
-    myMedia = new Media(mp3URL, playAudio, mediaError);
+    // var mp3URL = getMediaURL("sounds/fzn15.mp3");
+    var mp3URL = getMediaURL("sounds/zrrp.mp3");
+    myMedia = new Media(mp3URL, stopAudio, mediaError);
 
     // Update media position every second
-    var mediaTimer = setInterval(function() {
+    mediaTimer = setInterval(function() {
         // get media position
         myMedia.getCurrentPosition(
             // success callback
             function(position) {
                 if (position > -1) {
-                    document.getElementById('audio_position').innerHTML = Math.round(position) + " sec";
+                    positionVal = Math.round(position);
+                    document.getElementById('audio_position').innerHTML = positionVal.toHHMMSS();
                 }
             },
             // error callback
