a98941e1ddfd20dd84b0163807c16312ba2dbd70
Scott Roberson <sroberson@gmail.com>
2015-05-21 11:55:42 -0400

    replacing files

---
+++ www/js/index.js
@@ -0,0 +1,23 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function playMP3() {
+    var mp3URL = getMediaURL("sounds/button-1.mp3");
+    var media = new Media(mp3URL, null, mediaError);
+    media.play();
+}
+
+function getMediaURL(s) {
+    if(device.platform.toLowerCase() === "android") return "/android_asset/www/" + s;
+    return s;
+}
+
+function mediaError(e) {
+    alert('Media Error');
+    alert(JSON.stringify(e));
+}
+
+function onDeviceReady() {
+    playMP3();
+	document.querySelector("#playMp3").addEventListener("touchend", playMP3, false);
+};
+
